9ed143eda6d58b09b34c6257fa5957df58a23030
rraj1572 <91118622+rraj1572@users.noreply.github.com>
2022-07-04 12:37:38 +0530

    Add files via upload

--- mdiskdown.py
+++ mdiskdown.py
@@ -20,51 +20,7 @@ def echo(client, message):
 '''async def progress(current, total):
     await app.send_message(message.chat.id,f"{current * 100 / total:.1f}%")'''
 
-def down(v,a,message,link):
-    app.send_message(message.chat.id, 'downloading')
-    file = mdisk.mdow(link,v,a,message)
-    size = split.get_path_size(file)
-    if(size > 2097151000):
-        app.send_message(message.chat.id, 'spliting')
-        flist = split.split_file(file,size,file,".", TG_SPLIT_SIZE)
-        os.remove(file)
-        app.send_message(message.chat.id, 'uploading')
-        i = 1
-        for ele in flist:
-            app.send_document(message.chat.id,document=ele,caption=f"part {i}")#, progress=progress)
-            i = i + 1
-            os.remove(ele)
-    else:
-        app.send_message(message.chat.id, 'uploading')
-        app.send_document(message.chat.id,document=file)#, progress=progress)
-        os.remove(file)
-
-
-@app.on_message(filters.command(["mdisk"]))
-def echo(client, message):
-    try:
-        link = message.text.split("mdisk ")[1]
-        if "mdisk" in link:
-            out = mdisk.req(link)
-            app.send_message(message.chat.id, out)
-            app.send_message(message.chat.id, 'send VideoID,AudioID like this >> 0,1')
-            with open(f"{message.chat.id}.txt","w") as ci:
-                ci.write(link)
-    except:
-        app.send_message(message.chat.id, 'send only mdisk link with command followed by link')
-
-@app.on_message(filters.text)
-def echo(client, message):
-    if os.path.exists(f"{message.chat.id}.txt"):
-        with open(f"{message.chat.id}.txt","r") as li:
-            link = li.read()
-        link = link.split("\n")[0] 
-        os.remove(f"{message.chat.id}.txt")
-        ids = message.text.split(",")
-        d = threading.Thread(target=lambda:down(ids[0],ids[1],message,link),daemon=True)
-        d.start()
-        #await down(ids[0],ids[1],message,link)
-    else:
-        app.send_message(message.chat.id, "first send me link with /mdisk")
+def req(link):
+    app.send_message(message.chat.id, resp)
 
 app.run()    
